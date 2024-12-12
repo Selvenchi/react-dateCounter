81a1e34c33b708d9fd28e16419b213370a0c3f27
unknown <Selvenchi>
2024-12-12 20:45:42 +0700

    done 2

--- src/App.js
+++ src/App.js
@@ -12,39 +12,28 @@ export default function App() {
 function Counter() {
   const [num, setNum] = useState(0);
   const [step, setStep] = useState(1);
-  // const [date, setDate] = useState("June 21 2027");
-  // console.log(setDate);
 
-  // function numAdd() {
-  //   setNum((a) => a + step);
+  // function stepAdd() {
+  //   setStep((a) => a + 1);
   // }
 
-  function numSub() {
-    setNum((s) => s - step);
-  }
+  // function stepSub() {
+  //   setStep((s) => s - 1);
+  // }
 
-  function stepAdd() {
-    setStep((a) => a + 1);
-  }
-
-  function stepSub() {
-    setStep((s) => s - 1);
-  }
-
-  // Function to Add days to current date
   function addDays(date, days) {
     const newDate = new Date(date);
     newDate.setDate(date.getDate() + days);
     return newDate;
   }
 
-  // Get the current date
+  function init() {
+    setStep(1);
+    setNum(0);
+  }
+
   const todayDate = new Date();
-
-  // Number of days that we want to add to the current date
   const days = num;
-
-  // Function call to add days
   const newDate = addDays(todayDate, days);
 
   console.log("New Date: ", newDate.toDateString());
@@ -52,24 +41,32 @@ function Counter() {
   return (
     <div>
       <div>
+        <input
+          type="range"
+          min="0"
+          max="10"
+          value={step}
+          onChange={(e) => setStep(Number(e.target.value))}
+        ></input>
+        <div>Step: {step}</div>
+      </div>
+
+      {/* <div>
         <button onClick={stepSub}>-</button>
         <div>Step: {step}</div>
         <button onClick={stepAdd}>+</button>
-      </div>
+      </div> */}
 
       <div>
-        <button onClick={numSub}>-</button>
-        <div>Count: {num}</div>
+        <button onClick={() => setNum((a) => a - step)}>-</button>
+        {/* <div>Count: {num}</div> */}
+        <input
+          type="text"
+          value={num}
+          onChange={(e) => setNum(e.target.value)}
+        ></input>
         <button onClick={() => setNum((a) => a + step)}>+</button>
       </div>
-
-      {/* {!num ? (
-        <div>Today is {newDate.toDateString()}</div>
-      ) : (
-        <div>
-          {num} days from today is {newDate.toDateString()}
-        </div>
-      )} */}
 
       <p>
         <span>
@@ -81,6 +78,8 @@ function Counter() {
         </span>
         <span> {newDate.toDateString()}</span>
       </p>
+
+      <button onClick={init}>Reset</button>
     </div>
   );
 }
